856eb43b6d80d9a65f508ba1fc6671cbf33ffe5e
adam9500370 <adam9500370@yahoo.com.tw>
2016-01-04 01:43:52 +0800

    dataset.h

---
+++ dataset.h
@@ -0,0 +1,15 @@
+#ifndef __DATASET_H__
+#define __DATASET_H__
+class DataSet {
+public:
+	double vector[2];
+	double distance(const DataSet &b) const {
+		double sum = 0;
+		for(int i = 0; i < 2; i++) {
+			double d = vector[i] - b.vector[i];
+			sum += d*d; // 2-norm
+		}
+		return sum;
+	}
+}
+#endif
