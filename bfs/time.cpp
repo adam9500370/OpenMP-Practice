3fd318aaa9cdfc7894234f54841a7937f71c3708
adam9500370 <adam9500370@yahoo.com.tw>
2016-08-16 19:49:10 +0800

    add Linux_Time

--- bfs/time.cpp
+++ bfs/time.cpp
@@ -12,3 +12,16 @@ void Windows_Time::set_end_time() {
 double Windows_Time::get_time_interval() {
 	return (double) (end.QuadPart - start.QuadPart) / frequency.QuadPart;
 }
+
+
+void Linux_Time::set_start_time() {
+	gettimeofday(&start, NULL);
+}
+
+void Linux_Time::set_end_time() {
+	gettimeofday(&end, NULL);
+}
+
+double Linux_Time::get_time_interval() {
+	return (double) ( (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0 );
+}
--- bfs/time.h
+++ bfs/time.h
@@ -3,10 +3,20 @@
 
 #include "time_base.h"
 #include <windows.h>
+#include <sys/time.h>
 
-class Windows_Time : public Time_Base {	
+class Windows_Time : public Time_Base {
 	LARGE_INTEGER frequency, start, end;
-	double interval;
+	
+public:
+	void set_start_time();
+	void set_end_time();
+	double get_time_interval();
+};
+
+
+class Linux_Time : public Time_Base {
+	struct timeval start, end;
 	
 public:
 	void set_start_time();
