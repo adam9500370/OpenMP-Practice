6d97d4153f318ae4d5b26523aa355e0c2756c533
adam9500370 <adam9500370@yahoo.com.tw>
2016-08-24 14:59:52 +0800

    modify: change edge_list struct eliminate duplicate edge in edge_list vector<bool> -> vector<char> vector clear -> swap to empty vector

--- bfs/pbfs.cpp
+++ bfs/pbfs.cpp
@@ -14,29 +14,32 @@
 int N, M;
 
 std::vector<double> ii_arr, jj_arr, norm_arr;
-std::vector<bool> ii_bit, jj_bit;
+std::vector<char> ii_bit, jj_bit;
 std::vector<int> p_v, p_e;
 
-std::vector<int> edge_list[2]; // 0 for start vertex, 1 for end vertex
+struct Edge {
+	int x, y;
+};
+std::vector<Edge> edge_list, tmp_edge_list; // x for start vertex, y for end vertex
 std::vector<int> in_deg;
 std::vector<int> rand_search_key, search_key;
 std::vector<int> merge_queue;
 std::vector<Queue_Base*> next_queues;
-std::vector<bool> visited;
+std::vector<char> visited;
 std::vector<std::vector<int> > adjacency_list;
-std::vector<int> node_level, node_parent;
+std::vector<int> node_parent;
 
 std::vector<int> next_queue_size, current_queue_size;
 
-int max_threads = omp_get_max_threads();
-bool flag_merge;
+int max_threads/* = omp_get_max_threads()*/;
 
 void initialize() {
 	srand(time(NULL));
 	
+	omp_set_num_threads(max_threads);
+	
 	in_deg = std::vector<int> (N);
 	adjacency_list = std::vector<std::vector<int> > (N);
-	//node_level = std::vector<int> (N, -1);
 	
 	next_queues = std::vector<Queue_Base*> (max_threads);
 	for(int i = 0; i < next_queues.size(); i++) {
@@ -46,7 +49,6 @@ void initialize() {
 	}
 	
 	next_queue_size = std::vector<int> (N);
-	current_queue_size = std::vector<int> (N);
 }
 
 void randperm(int n, std::vector<int> &perm) {
@@ -70,13 +72,13 @@ void rand_arr(int len, std::vector<double> &arr) {
 	}
 }
 
-void cmp_gt(int len, const std::vector<double> &arr1, const std::vector<double> &arr2, std::vector<bool> &result_arr) { // compare whether arr1 > arr2 or not
+void cmp_gt(int len, const std::vector<double> &arr1, const std::vector<double> &arr2, std::vector<char> &result_arr) { // compare whether arr1 > arr2 or not
 	for(int i = 0; i < len; i++) {
 		result_arr[i] = (arr1[i] > arr2[i]);
 	}
 }
 
-void cmp_gt(int len, const std::vector<double> &arr1, double value, std::vector<bool> &result_arr) { // compare whether arr1 > value or not
+void cmp_gt(int len, const std::vector<double> &arr1, double value, std::vector<char> &result_arr) { // compare whether arr1 > value or not
 	for(int i = 0; i < len; i++) {
 		result_arr[i] = (arr1[i] > value);
 	}
@@ -95,8 +97,8 @@ void kronecker_generator(int SCALE, int edgefactor) {
 	double A = 0.57, B = 0.19, C = 0.19;
 	
 	// Create index arrays.
-	edge_list[0] = std::vector<int> (M);
-	edge_list[1] = std::vector<int> (M);
+	tmp_edge_list = std::vector<Edge> (M);
+	
 	// Loop over each order of bit.
 	double ab, c_norm, a_norm;
 	ab = A + B;
@@ -105,11 +107,11 @@ void kronecker_generator(int SCALE, int edgefactor) {
 	
 	//printf("N: %d, M: %d, ab: %f, c_norm: %f, a_norm: %f\n", N, M, ab, c_norm, a_norm);
 	
-	ii_arr = std::vector<double> (M);
-	jj_arr = std::vector<double> (M);
-	norm_arr = std::vector<double> (M);
-	ii_bit = std::vector<bool> (M);
-	jj_bit = std::vector<bool> (M);
+	ii_arr.reserve(M);
+	jj_arr.reserve(M);
+	norm_arr.reserve(M);
+	ii_bit.reserve(M);
+	jj_bit.reserve(M);
 	for(int ib = 1; ib <= SCALE; ib++) {
 		// Compare with probabilities and set bits of indices.
 		rand_arr(M, ii_arr);
@@ -120,91 +122,64 @@ void kronecker_generator(int SCALE, int edgefactor) {
 		}
 		cmp_gt(M, jj_arr, norm_arr, jj_bit);
 		for(int k = 0; k < M; k++) {
-			edge_list[0][k] += pow(2, (ib-1)) * ii_bit[k];
-			edge_list[1][k] += pow(2, (ib-1)) * jj_bit[k];
+			tmp_edge_list[k].x += pow(2, (ib-1)) * ii_bit[k];
+			tmp_edge_list[k].y += pow(2, (ib-1)) * jj_bit[k];
 		}
 	}
-	ii_arr.clear();
-	jj_arr.clear();
-	norm_arr.clear();
-	ii_bit.clear();
-	jj_bit.clear();
+	std::vector<double> ().swap(ii_arr);
+	std::vector<double> ().swap(jj_arr);
+	std::vector<double> ().swap(norm_arr);
+	std::vector<char> ().swap(ii_bit);
+	std::vector<char> ().swap(jj_bit);
 	
 	// Permute vertex labels
-	p_v = std::vector<int> (N);
+	p_v.reserve(N);
 	randperm(N, p_v);
-	for(int i = 0; i < 2; i++) {
-		for(int k = 0; k < M; k++) {
-			edge_list[i][k] = p_v[edge_list[i][k]];
-		}
+	for(int k = 0; k < M; k++) {
+		tmp_edge_list[k].x = p_v[tmp_edge_list[k].x];
+		tmp_edge_list[k].y = p_v[tmp_edge_list[k].y];
 	}
-	p_v.clear();
+	std::vector<int> ().swap(p_v);
 	
 	// Permute the edge list
-	p_e = std::vector<int> (M);
+	edge_list = std::vector<Edge> (M);
+	p_e.reserve(M);
 	randperm(M, p_e);
-	for(int i = 0; i < 2; i++) {
-		for(int k = 0; k < M; k++) {
-			edge_list[i][k] = edge_list[i][p_e[k]];
-		}
+	for(int k = 0; k < M; k++) {
+		edge_list[k].x = tmp_edge_list[p_e[k]].x;
+		edge_list[k].y = tmp_edge_list[p_e[k]].y;
 	}
-	p_e.clear();
+	std::vector<int> ().swap(p_e);
+	std::vector<Edge> ().swap(tmp_edge_list);
 }
 
-void read_edges() {
-	freopen("in", "r", stdin);
-	
-	int start_node, end_node;
-	scanf("%d", &N);
-	initialize();
-	while(scanf("%d%d", &start_node, &end_node) != EOF) {
-		edge_list[0].push_back(start_node);
-		edge_list[1].push_back(end_node);
-	}
+bool cmp_edge(const Edge &a, const Edge &b) {
+    return (a.x < b.x) || (a.x == b.x && a.y < b.y);
 }
 
-void read_lists() {
-	freopen("in", "r", stdin);
-	
-	int node_deg, end_node;
-	scanf("%d", &N);
-	initialize();
-	for(int start_node = 0; start_node < N; start_node++) {
-		scanf("%d", &node_deg);
-		for(int i = 0; i < node_deg; i++) {
-			scanf("%d", &end_node);
-			edge_list[0].push_back(start_node);
-			edge_list[1].push_back(end_node);
-		}
-	}
+bool test_self_edge(int index) {
+	return (edge_list[index].x == edge_list[index].y);
 }
 
-void read_graph() {
-	freopen("in.graph", "r", stdin);
-	
-	int end_node;
-	std::string line;
-	scanf("%d%d%c", &N, &M);
-	initialize();
-	for(int start_node = 0; start_node < N; start_node++) {
-		getline(std::cin, line);
-		std::istringstream in(line);
-		while(in >> end_node) {
-			edge_list[0].push_back(start_node);
-			edge_list[1].push_back(end_node-1);
-		}
-	}
+bool test_duplicate_edge(int index) {
+	return ((index > 0) && (edge_list[index].x == edge_list[index-1].x) && (edge_list[index].y == edge_list[index-1].y));
 }
 
 /*** transform edge list into adjacency list ***/
-void construct_graph(bool flag_duplicate) {
-	for(int i = 0; i < edge_list[0].size(); i++) {
-		if(edge_list[0][i] != edge_list[1][i]) { // no self edge, undirected graph
-			adjacency_list[edge_list[0][i]].push_back(edge_list[1][i]);
-			in_deg[edge_list[1][i]]++;
-			if(flag_duplicate) {
-				adjacency_list[edge_list[1][i]].push_back(edge_list[0][i]);
-				in_deg[edge_list[0][i]]++;
+void construct_graph(bool flag_bidirection) {
+	sort(edge_list.begin(), edge_list.end(), cmp_edge);
+	for(int i = 0; i < edge_list.size(); i++) {
+		if(!test_self_edge(i)) { // no self edge, no duplicate edge, undirected graph
+			in_deg[edge_list[i].y]++;
+			if(flag_bidirection) {
+				in_deg[edge_list[i].x]++;
+			}
+			
+			if(!test_duplicate_edge(i)) {
+				adjacency_list[edge_list[i].x].push_back(edge_list[i].y);				
+				if(flag_bidirection) {
+					adjacency_list[edge_list[i].y].push_back(edge_list[i].x);
+				}
 			}
 		}
 	}
@@ -254,14 +229,17 @@ void merge_to_merge_queue(int level) {
 	}
 }
 
-void pbfs(int start_point/*, double* time_group*/) {
+void pbfs(int start_point/*, double* time_group, int group_num*/) {
 	int level = 1;
-	/*Linux_Time *timer_1 = new Linux_Time, *timer_2 = new Linux_Time, *timer_3 = new Linux_Time;
+	/*
+	std::vector<Windows_Time*> timer.reserve(group_num);
 	
-	time_group[0] = 0.0;
-	time_group[1] = 0.0;
-	time_group[2] = 0.0;*/
-	
+	for(int i = 0; i < group_num; i++) {
+		timer[i] = new Windows_Time;
+		time_group[i] = 0.0;
+	}
+	*/
+	//timer[0]->set_start_time();
 	node_parent.clear();
 	node_parent.reserve(N);
 	
@@ -271,7 +249,6 @@ void pbfs(int start_point/*, double* time_group*/) {
 	merge_queue.clear();
 	merge_queue.push_back(start_point);
 	
-	omp_set_num_threads(max_threads);
 	#pragma omp parallel
 	{
 		#pragma omp for nowait
@@ -286,23 +263,25 @@ void pbfs(int start_point/*, double* time_group*/) {
 	
 	node_parent[start_point] = start_point;
 	next_queue_size[0] = 1;
+	/*timer[0]->set_end_time();
+	time_group[0] += timer[0]->get_time_interval();*/
 	
 	while(next_queue_size[level-1] > 0) {
-		//timer_1->set_start_time();
+		//timer[1]->set_start_time();
 		find_next_queues_by_merge_queue(level);
-		/*timer_1->set_end_time();
-		time_group[0] += timer_1->get_time_interval();*/
-			
-		//timer_2->set_start_time();
+		/*timer[1]->set_end_time();
+		time_group[1] += timer[1]->get_time_interval();*/
+		
+		//timer[2]->set_start_time();
 		record_next_queues_size(level);
-		/*timer_2->set_end_time();
-		time_group[1] += timer_2->get_time_interval();*/
-			
-		//timer_3->set_start_time();
+		/*timer[2]->set_end_time();
+		time_group[2] += timer[2]->get_time_interval();*/
+		
+		//timer[3]->set_start_time();
 		merge_to_merge_queue(level);
-		/*timer_3->set_end_time();
-		time_group[2] += timer_3->get_time_interval();*/
-			
+		/*timer[3]->set_end_time();
+		time_group[3] += timer[3]->get_time_interval();*/
+		
 		level++;
 	}
 	
@@ -326,9 +305,8 @@ bool validate(int start_point) {
 	}
 	
 	int current_level = 1;
-	node_level = std::vector<int> (N);
-	std::vector<int> current_level_list, next_level_list;
-	next_level_list.clear();
+	std::vector<int> node_level, current_level_list, next_level_list;
+	node_level.reserve(N);
 	next_level_list.reserve(N);
 	for(int i = 0; i < N; i++) {
 		if(node_parent[i] != -1) {
@@ -359,19 +337,20 @@ bool validate(int start_point) {
 		}
 		current_level_list = next_level_list;
 	}
+	std::vector<int> ().swap(next_level_list);
 	
-	for(int i = 0; i < edge_list[0].size(); i++) {
-		if( ! (	(node_level[edge_list[0][i]] == -1 && node_level[edge_list[1][i]] == -1) // both out of graph
-			 || (node_level[edge_list[0][i]] != -1 && node_level[edge_list[1][i]] != -1) // both in graph
-			 || (abs(node_level[edge_list[0][i]] - node_level[edge_list[1][i]]) <= 1) // each tree edge connects vertices whose BFS levels differ by exactly one
+	for(int i = 0; i < edge_list.size(); i++) {
+		if( ! (	(node_level[edge_list[i].x] == -1 && node_level[edge_list[i].y] == -1) // both out of graph
+			 || (node_level[edge_list[i].x] != -1 && node_level[edge_list[i].y] != -1) // both in graph
+			 || (abs(node_level[edge_list[i].x] - node_level[edge_list[i].y]) <= 1) // each tree edge connects vertices whose BFS levels differ by exactly one
 				)){
 			printf("err: -3\n");
-			printf("e: %d %d, l: %d %d\n", edge_list[0][i], edge_list[1][i], node_level[edge_list[0][i]], node_level[edge_list[1][i]]);
+			printf("e: %d %d, l: %d %d\n", edge_list[i].x, edge_list[i].y, node_level[edge_list[i].x], node_level[edge_list[i].y]);
 			return false;
 		}
 	}
+	std::vector<int> ().swap(node_level);
 	
-	node_level.clear();
 	return true;
 }
 
@@ -417,12 +396,13 @@ void statistics(double* value, int NBFS, double* result_info) {
 	result_info[6] = stddev;
 }
 
-void output(int SCALE, int edgefactor, int NBFS, double kernel_1_time, double* kernel_2_time, double* kernel_2_nedge/*, double* total_time_group*/) {
-	char strch_SCALE[10], strch_edgefactor[10];
+void output(int SCALE, int edgefactor, int NBFS, double kernel_1_time, double* kernel_2_time, double* kernel_2_nedge/*, double* total_time_group, int group_num*/) {
+	char strch_max_threads[10], strch_SCALE[10], strch_edgefactor[10];
+	sprintf(strch_max_threads, "%d", max_threads);
 	sprintf(strch_SCALE, "%d", SCALE);
 	sprintf(strch_edgefactor, "%d", edgefactor);
-	std::string str_SCALE(strch_SCALE), str_edgefactor(strch_edgefactor);
-	std::string file_name = "SCALE=" + str_SCALE + "_edgefactor=" + str_edgefactor;
+	std::string str_max_threads(strch_max_threads), str_SCALE(strch_SCALE), str_edgefactor(strch_edgefactor);
+	std::string file_name = "Test_OpenMP_" + str_max_threads + "threads_Kronecker/" + "SCALE=" + str_SCALE + "_edgefactor=" + str_edgefactor; // store in Test_OpenMP_Xthreads_Kronecker dir
 	freopen(file_name.data(), "w", stdout);
 	
 	printf("SCALE: %d\n", SCALE);
@@ -488,21 +468,43 @@ void output(int SCALE, int edgefactor, int NBFS, double kernel_1_time, double* k
 	printf("max_TEPS: %20.17e\n", S[4]);
 	printf("harmonic_mean_TEPS: %20.17e\n", S[5]);
 	printf("harmonic_stddev_TEPS: %20.17e\n", S[6]);
-	
-	/*printf("\naverage_time_group[0]: %f (%f%%)\naverage_time_group[1]: %f (%f%%)\naverage_time_group[2]: %f (%f%%)\n"
-		, total_time_group[0]/NBFS, (total_time_group[0]/NBFS)/kernel_2_mean_time*100
-		, total_time_group[1]/NBFS, (total_time_group[1]/NBFS)/kernel_2_mean_time*100
-		, total_time_group[2]/NBFS, (total_time_group[2]/NBFS)/kernel_2_mean_time*100);*/
+	/*
+	printf("\n");
+	for(int i = 0; i < group_num; i++) {
+		printf("average_time_group[%d]: %f (%f%%)\n", i, total_time_group[i]/NBFS, (total_time_group[i]/NBFS)/kernel_2_mean_time*100);
+	}
+	*/
+	/*
+	printf("\n");
+	for(int i = 0; i < N; i++) {
+		if(in_deg[i])
+			printf("in_deg[%6d] = %6d\n", i, in_deg[i]);
+	}
+	*/
+	/*
+	printf("\n");
+	for(int i = 0; i < N; i++) {
+		if(adjacency_list[i].size())
+			printf("adjacency_list[%6d].size = %6d\n", i, adjacency_list[i].size());
+	}
+	*/
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 	int SCALE, edgefactor;
 	int NBFS = 64;
-	Linux_Time *timer_kernel_1 = new Linux_Time, *timer_kernel_2 = new Linux_Time;
-	double kernel_1_time, kernel_2_time[NBFS], kernel_2_nedge[NBFS];
+	Windows_Time *timer_kernel_1 = new Windows_Time, *timer_kernel_2 = new Windows_Time;
+	double kernel_1_time, kernel_2_time[NBFS] = {0.0}, kernel_2_nedge[NBFS] = {0.0};
 	
-	printf("Please enter SCALE and edgefactor:\n");
-	scanf("%d%d", &SCALE, &edgefactor);
+	if(argc == 4) { // argv[0] = pbfs
+		max_threads = atoi(argv[1]);
+		SCALE = atoi(argv[2]);
+		edgefactor = atoi(argv[3]);
+	}
+	else {
+		exit(EXIT_FAILURE);
+	}
+	
 	kronecker_generator(SCALE, edgefactor);
 	
 	//read_edges();
@@ -518,30 +520,32 @@ int main() {
 	kernel_1_time = timer_kernel_1->get_time_interval();
 	//printf("kernel_1_time: %f\n", kernel_1_time);
 	
-	rand_search_key = std::vector<int> (N);
+	rand_search_key.reserve(N);
 	randperm(N, rand_search_key);
 	for(int i = 0; i < N; i++) {
 		if(adjacency_list[rand_search_key[i]].size() > 0) {
 			search_key.push_back(rand_search_key[i]);
 		}
 	}
-	rand_search_key.clear();
+	std::vector<int> ().swap(rand_search_key);
 	if(search_key.size() > NBFS) {
 		search_key.erase(search_key.end()-(N-NBFS), search_key.end());
 	}
 	else {
 		NBFS = search_key.size();
 	}
-	
-	//double time_group[3], total_time_group[3] = {0.0};
+	/*
+	int group_num = 4;
+	double time_group[group_num], total_time_group[group_num] = {0.0};
+	*/
 	for(int i = 0; i < NBFS; i++) {
 		//printf("search key: %d\n", search_key[i]);		
 		timer_kernel_2->set_start_time();
-		pbfs(search_key[i]/*, time_group*/);	
+		pbfs(search_key[i]/*, time_group, group_num*/);	
 		timer_kernel_2->set_end_time();
 		kernel_2_time[i] = timer_kernel_2->get_time_interval();
 		//printf("kernel_2_time[%d]: %f\n", i, kernel_2_time[i]);
-		/*for(int j = 0; j < 3; j++) {
+		/*for(int j = 0; j < group_num; j++) {
 			total_time_group[j] += time_group[j];
 		}*/
 		
@@ -550,7 +554,7 @@ int main() {
 			break;
 		}
 		
-		kernel_2_nedge[i] = 0;
+		kernel_2_nedge[i] = 0.0;
 		for(int j = 0; j < N; j++) {
 			if(node_parent[j] != -1) {
 				kernel_2_nedge[i] += in_deg[j];
@@ -559,6 +563,6 @@ int main() {
 		kernel_2_nedge[i] /= 2;
 	}
 	
-	output(SCALE, edgefactor, NBFS, kernel_1_time, kernel_2_time, kernel_2_nedge/*, total_time_group*/);
+	output(SCALE, edgefactor, NBFS, kernel_1_time, kernel_2_time, kernel_2_nedge/*, total_time_group, group_num*/);
 	return 0;
 }
